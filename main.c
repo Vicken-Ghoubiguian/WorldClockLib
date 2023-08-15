78f4e5864215f142fdb24f3b6ca3e3462a1496da
Eric ghoubiguian <eric.ghoubiguian@imerir.com>
2023-08-15 14:52:45 +0200

    To beggin implementation for timezones

--- main.c
+++ main.c
@@ -1,24 +1,41 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
+#include <string.h>
 
-time_t getDateTime_Epoch() {
+#define __NULL ((void *)0)
+
+time_t getDateTime_Epoch(char* timezone) {
 
 	time_t utc_now = time(NULL);
 
-	return utc_now;
+	if(strcmp(timezone, "UTC") == 0)
+	{
+		return utc_now;
+	}
+	else
+	{
+		return -1;
+	}
 }
 
-struct tm *getDateTime_Tm() {
+struct tm *getDateTime_Tm(char* timezone) {
 
 	time_t utc_now = time(NULL);
 
-	return gmtime(&utc_now);
+	if(strcmp(timezone, "UTC") == 0)
+        {
+                return gmtime(&utc_now);
+        }
+        else
+        {
+                return __NULL;
+        }
 }
 
 int main() {
 
-	printf("Current UTC time is  %ld (EPOCH)\n", getDateTime_Epoch());
+	printf("Current UTC time is  %ld (EPOCH)\n", getDateTime_Epoch("UTC"));
 
 	return 0;
 }
