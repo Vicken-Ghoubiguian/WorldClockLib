12a4b4acf9fa92475b79cd1260381e5069cc955c
Eric ghoubiguian <eric.ghoubiguian@imerir.com>
2023-08-15 15:21:27 +0200

    For struct tm*

--- main.c
+++ main.c
@@ -13,6 +13,18 @@ time_t getDateTime_Epoch(char* timezone) {
 	{
 		return utc_now;
 	}
+	else if(strcmp(timezone, "Africa/Abidjan") == 0)
+	{
+		return utc_now;
+	}
+	else if(strcmp(timezone, "Africa/Algiers") == 0)
+        {
+                return utc_now + 3600;
+        }
+	else if(strcmp(timezone, "Africa/Bissau") == 0)
+        {
+                return utc_now + 3600;
+        }
 	else
 	{
 		return -1;
@@ -22,10 +34,25 @@ time_t getDateTime_Epoch(char* timezone) {
 struct tm *getDateTime_Tm(char* timezone) {
 
 	time_t utc_now = time(NULL);
+	time_t tz_now;
 
 	if(strcmp(timezone, "UTC") == 0)
         {
                 return gmtime(&utc_now);
+        }
+	else if(strcmp(timezone, "Africa/Abidjan") == 0)
+        {
+                return gmtime(&utc_now);
+        }
+        else if(strcmp(timezone, "Africa/Algiers") == 0)
+        {
+                tz_now = utc_now + 3600;
+		return gmtime(&tz_now);
+        }
+        else if(strcmp(timezone, "Africa/Bissau") == 0)
+        {
+                tz_now = utc_now + 3600;
+		return gmtime(&tz_now);
         }
         else
         {
@@ -35,7 +62,7 @@ struct tm *getDateTime_Tm(char* timezone) {
 
 int main() {
 
-	printf("Current UTC time is  %ld (EPOCH)\n", getDateTime_Epoch("UTC"));
+	printf("Current UTC time is  %ld (EPOCH)\n", getDateTime_Epoch("Africa/Algiers"));
 
 	return 0;
 }
