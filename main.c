ba3546487eb2aeb6bae675c5a8c8f2e6fe058cfe
Eric ghoubiguian <eric.ghoubiguian@imerir.com>
2023-08-18 07:44:39 +0200

    Summyz

--- main.c
+++ main.c
@@ -19,6 +19,7 @@ time_t getDateTime_Epoch(char* timezone) {
 	}
 	else if(strcmp(timezone, "Africa/Algiers") == 0)
         {
+		printf("%ld", utc_now + 3600);
                 return utc_now + 3600;
         }
 	else if(strcmp(timezone, "Africa/Bissau") == 0)
@@ -42,7 +43,7 @@ time_t getDateTime_Epoch(char* timezone) {
 struct tm *getDateTime_Tm(char* timezone) {
 
 	time_t utc_now = time(NULL);
-	time_t tz_now;
+	//time_t tz_now;
 
 	if(strcmp(timezone, "UTC") == 0)
         {
@@ -54,22 +55,23 @@ struct tm *getDateTime_Tm(char* timezone) {
         }
         else if(strcmp(timezone, "Africa/Algiers") == 0)
         {
-                tz_now = utc_now + 3600;
+                time_t tz_now = utc_now + 3600;
+		printf("%ld", tz_now);
 		return gmtime(&tz_now);
         }
         else if(strcmp(timezone, "Africa/Bissau") == 0)
         {
-                tz_now = utc_now + 3600;
+                time_t tz_now = utc_now + 3600;
 		return gmtime(&tz_now);
         }
 	else if(strcmp(timezone, "Asia/Seoul") == 0)
         {
-                tz_now = utc_now + 32400;
+                time_t tz_now = utc_now + 32400;
 		return gmtime(&tz_now);
         }
         else if(strcmp(timezone, "Asia/Pyongyang") == 0)
         {
-                tz_now = utc_now + 32400;
+                time_t tz_now = utc_now + 32400;
 		return gmtime(&tz_now);
         }
         else
@@ -80,8 +82,12 @@ struct tm *getDateTime_Tm(char* timezone) {
 
 int main() {
 
-	//printf("Current UTC time is  %s\n", ctime(*getDateTime_Epoch("Africa/Algiers")));
-	printf("Current UTC time is  %s\n", asctime(getDateTime_Tm("Africa/Algiers")));
+	time_t timestampGetDateTime = getDateTime_Epoch("Africa/Algiers");
+	printf("Current UTC time is  %s\n", ctime(&timestampGetDateTime));
+
+	struct tm *tmGetDateTime = getDateTime_Tm("Africa/Algiers");	
+	printf("%d", tmGetDateTime->tm_hour);
+	//printf("Current UTC time is  %s\n", asctime(tmGetDateTime));
 
 	return 0;
 }
