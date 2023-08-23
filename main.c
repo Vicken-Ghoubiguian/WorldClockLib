30ad21d169b971ee56b5fdeb934a0b201d9c069c
Eric ghoubiguian <eric.ghoubiguian@imerir.com>
2023-08-23 10:28:50 +0200

    For some tests for bug resolution...

--- main.c
+++ main.c
@@ -6,7 +6,12 @@ int main() {
 	/*time_t timestampGetDateTime = getDateTime_Epoch("Africa/Algiers");
 	printf("Current UTC time is %s\n", ctime(&timestampGetDateTime));*/
 
-	struct tm *tmGetDateTime = getDateTime_Tm("Asia/Shanghai");
+	/*char tz[20];
+	printf("Veuillez saisir le timezone que vous voulez : ");
+	scanf("%s", tz);
+	printf("\n");*/
+
+	struct tm *tmGetDateTime = getDateTime_Tm("Pacific/Marquesas");
 	printf("Current UTC time is %s\n", asctime(tmGetDateTime));
 
 	return 0;
