01dcaff999dde38555df34837848c184cb94adc5
Eric ghoubiguian <eric.ghoubiguian@imerir.com>
2023-10-26 15:12:07 +0200

    For SP

--- main.c
+++ main.c
@@ -14,11 +14,11 @@ int main() {
 	printf("%s", tz);
 
 	//
-	time_t timestampGetDateTime = getDateTime_Epoch("America/Rio_Branco");
+	time_t timestampGetDateTime = getDateTime_Epoch("America/Sao_Paulo");
 	printf("Current UTC time is %ld\n", timestampGetDateTime);
 
 	//
-	struct tm *tmGetDateTime = getDateTime_Tm("America/Rio_Branco");
+	struct tm *tmGetDateTime = getDateTime_Tm("America/Sao_Paulo");
 	printf("Current UTC time is %s\n", asctime(tmGetDateTime));
 
 	//
