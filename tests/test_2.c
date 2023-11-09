d2858fdc548166756f1ffdd5cd2aa065a9d04e67
Eric ghoubiguian <eric.ghoubiguian@imerir.com>
2023-11-09 10:38:49 +0100

    Completude 2

--- tests/test_2.c
+++ tests/test_2.c
@@ -16,7 +16,7 @@ int main() {
     printf("Date de changement à l'heure d'hiver concernant l'Australie pour l'année %d\n", utc_date_tm->tm_year + 1900);
 
     //
-    
+    printf("Date de changement à l'heure d'été concernant l'Australie pour l'année %d\n", utc_date_tm->tm_year + 1900);
 
     //
     return 0;
