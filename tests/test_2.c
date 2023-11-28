e2e05118552a82aae402fbc14e875e96e843c283
Eric ghoubiguian <eric.ghoubiguian@imerir.com>
2023-11-28 13:48:03 +0100

    Delete useless comments

--- tests/test_2.c
+++ tests/test_2.c
@@ -26,24 +26,7 @@ time_t first_sunday_in_choosen_month(time_t today, int month, int hour)
     date_tm = gmtime(&date_timestamp);
 
     //
-    /*while(1)
-    {
-        //
-        if(date_tm->tm_wday == 0 && date_tm->tm_mon == month)
-        {
-            break;
-		}
-		else
-		{
-			//
-            date_timestamp = date_timestamp + NB_SECONDS_IN_DAY;
-			
-            //
-            date_tm = gmtime(&date_timestamp);
-        }
-    }*/
-
-    while(date_tm->tm_wday != 0 /*&& date_tm->tm_mon != month*/)
+    while(date_tm->tm_wday != 0)
     {
         //
         date_timestamp = date_timestamp + NB_SECONDS_IN_DAY;
