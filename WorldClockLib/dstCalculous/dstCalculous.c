ff5fc0317b734e74b885e70e7596ec67eb6464a6
Eric ghoubiguian <eric.ghoubiguian@imerir.com>
2024-01-17 09:08:37 +0100

    Rewrite the function... Yes...

--- WorldClockLib/dstCalculous/dstCalculous.c
+++ WorldClockLib/dstCalculous/dstCalculous.c
@@ -1,55 +1,139 @@
 #include <time.h>
 #include "dstCalculous.h"
 
-//
-//#define NB_SECONDS_IN_DAY 86400
-
-//
-/*time_t first_sunday_in_choosen_month(time_t today, int month, int hour)
+// Definition of the 'wished_wday_in_choosen_month' function to get the date of the wished week day in the wished month and the wished year
+time_t wished_wday_in_choosen_month(int year, int month, int wday, int hour, int minute, int second, enum numeral num)
 {
     //
+    int i = 0;
+    int condition = 0;
+
+    // Definition of the 'date_tm' and the 'date_timestamp' variables
     struct tm *date_tm;
     time_t date_timestamp;
-    int i;
+
+    // Definition of the 'today' variable and its initialization
+    time_t today = time(NULL);
 
     //
     date_tm = gmtime(&today);
 
-    //
+    // Initialization of the 'date_tm' to make all the necessary calculations
+    date_tm->tm_year = year - 1900;
     date_tm->tm_mday = 1;
-    date_tm->tm_mon = month;
     date_tm->tm_hour = hour;
-    date_tm->tm_min = 0;
-    date_tm->tm_sec = 0;
+    date_tm->tm_min = minute;
+    date_tm->tm_sec = second;
+
+    //
+    if(num == LAST || num == BEFORE_LAST){ date_tm->tm_mon = month + 1; }
+    else{ date_tm->tm_mon = month; }
 
     //
     date_timestamp = timegm(date_tm);
     date_tm = gmtime(&date_timestamp);
 
     //
-    while(1)
+    if((num == LAST) || (num == FIRST))
     {
         //
-        if(date_tm->tm_wday == 0 && date_tm->tm_mon == month)
-        {
-            break;
-		}
-		else
-		{
-			//
-            date_timestamp = date_timestamp + NB_SECONDS_IN_DAY;
-			
-            //
-            date_tm = gmtime(&date_timestamp);
+        condition = date_tm->tm_wday != wday || date_tm->tm_mon != month;
+    }
+    //
+    else if(num == BEFORE_LAST)
+    {
+        //
+        condition = date_tm->tm_wday != wday || date_tm->tm_mon != month || i != 1;
+    }
+    //
+    else if(num == SECOND)
+    {
+        //
+        condition = date_tm->tm_wday != wday || i != 1;
+    }
+    //
+    else if(num == THIRD)
+    {
+        //
+        condition = date_tm->tm_wday != wday || i != 2;
+    }
 
-			//
-            if(date_tm->tm_wday == 0 && date_tm->tm_mon == month)
+    //
+    if((num == LAST) && (date_tm->tm_wday == 0))
+    {
+        //
+        date_timestamp = date_timestamp - NB_SECONDS_IN_DAY;
+
+        //
+        date_tm = gmtime(&date_timestamp);
+    }
+
+    //
+    while(condition)
+    {
+        //
+        if(num == BEFORE_LAST)
+        {
+            //
+            if(date_tm->tm_wday == wday && date_tm->tm_mon == month)
             {
+                //
                 i = i + 1;
             }
+        }
+        //
+        else if(num == SECOND || num == THIRD)
+        {
+            //
+            if(date_tm->tm_wday == wday)
+            {
+                //
+                i = i + 1;
+            }
+        }
+
+        //
+        if(num == LAST || num == BEFORE_LAST)
+        {
+            //
+            date_timestamp = date_timestamp - NB_SECONDS_IN_DAY;
+        }
+        //
+        else
+        {
+            //
+            date_timestamp = date_timestamp + NB_SECONDS_IN_DAY;
+        }
+
+        //
+        date_tm = gmtime(&date_timestamp);
+
+        //
+        if((num == LAST) || (num == FIRST))
+        {
+            //
+            condition = date_tm->tm_wday != wday || date_tm->tm_mon != month;
+        }
+        //
+        else if(num == BEFORE_LAST)
+        {
+            //
+            condition = date_tm->tm_wday != wday || date_tm->tm_mon != month || i != 1;
+        }
+        //
+        else if(num == SECOND)
+        {
+            //
+            condition = date_tm->tm_wday != wday || i != 1;
+        }
+        //
+        else if(num == THIRD)
+        {
+            //
+            condition = date_tm->tm_wday != wday || i != 2;
         }
     }
 
     //
     return date_timestamp;
-}*/
+}
